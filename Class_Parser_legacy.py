9d0945a28b2f2f77396541e4e553edaee0a73273
Preetham N <preetham.in.1997@gmail.com>
2016-11-28 18:10:57 -0800

    Added ability to search for specific class lecture when there are multiple lectures

--- Class_Parser_legacy.py
+++ Class_Parser_legacy.py
@@ -6,20 +6,21 @@ Send email notification when class opens
 """
 
 # Usage:
-# Replace the classes in the classes list with any class you want to search.
+# Replace the classes in the classes list with any class you want to search and the lecture number (0 for any lecture).
 # Change the quarter to the quarter you want the search.
 # NOTE: The class name must be the exact short form listed on the registrar.
 
 
-import os
+import os # pausing the command prompt
 
-import re
+import re # parse the input string using regex
 
+# parse the registrar website
 from bs4 import BeautifulSoup
 import urllib
 
-# Example: This checks the classes COM SCI M117, COM SCI 131, and COM SCI 174A for the 17W quarter
-classes = ['COM SCI M117', 'COM SCI 131', 'COM SCI 174A']
+# Example: This checks the classes COM SCI 174A (any lecture) and STATS 10 (LEC 2) for the 17W quarter
+classes = [('COM SCI 174A', 0), ('STATS 10', 5)]
 quarter = '17W'
 
 def get_class(class_name):
@@ -35,23 +36,36 @@ def get_class(class_name):
 	number = number+last+first
 	return (subject, number)
 
+def check_lecture(lecture):
+	class_status = lecture.nextSibling
+	capacity = int(class_status.find('td', {'class', 'dgdClassDataEnrollCap'}).text)
+	enrolled = int(class_status.find('td', {'class', 'dgdClassDataEnrollTotal'}).text)
+	wait_capacity = int(class_status.find('td', {'class', 'dgdClassDataWaitListCap'}).text)
+	wait_enrolled = int(class_status.find('td', {'class', 'dgdClassDataWaitListTotal'}).text)
+	if enrolled < capacity:
+		class_found_flag = True
+		print course
+	elif wait_enrolled < wait_capacity:
+		class_found_flag = True
+		print course + ": Wait"
 
 class_found_flag = False
 
-for course in classes:
+for course, lec in classes:
 	(subject, number) = get_class(course)
 	URL = 'http://legacy.registrar.ucla.edu/schedule/detselect.aspx?termsel={0}&subareasel={1}&idxcrs={2}'.format(quarter, subject, number)
 	soup = BeautifulSoup(urllib.urlopen(URL))
 	
 	lectures = soup.find_all('tr', {'class','dgdClassDataHeader'})
 	
-	for lecture in lectures:
-		class_status = lecture.nextSibling
-		capacity = int(class_status.find('td', {'class', 'dgdClassDataEnrollCap'}).text)
-		enrolled = int(class_status.find('td', {'class', 'dgdClassDataEnrollTotal'}).text)
-		if enrolled < capacity:
-			class_found_flag = True
-			print course
+	if lec == 0:
+		for lecture in lectures:
+			check_lecture(lecture)
+	else:
+		try:
+			check_lecture(lectures[lec-1])
+		except IndexError:
+			print "Error: lecture number is out of range"
 
 
 if class_found_flag:
